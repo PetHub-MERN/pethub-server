4dfc47ef34f41c909cdbc814bb3646d0267910b7
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-16 15:16:53 +0100

    Clean isNotAuthenticated function

--- middleware/isNotAuthenticated.js
+++ middleware/isNotAuthenticated.js
@@ -3,7 +3,6 @@ const isNotAuthenticated = (req, res, next) => {
     if(req.payload === undefined) {
         next();
     } else {
-        console.log(req.payload);
         res.status(400).json({ message: "You are already logged in!" })
     }
 
