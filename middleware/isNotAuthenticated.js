77e2d159fb310f62a2ba738aa47fbccc75e6d8e3
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-18 09:52:14 +0100

    Fix isNotAuthenticated middleware

--- middleware/isNotAuthenticated.js
+++ middleware/isNotAuthenticated.js
@@ -1,8 +1,19 @@
+const jwt = require("jsonwebtoken");
+
 const isNotAuthenticated = (req, res, next) => {
 
     if(req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
 
-        res.status(400).json({message: "You are already Logged In!"})
+        const token = req.headers.authorization.split(" ")[1];
+
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedToken) => {
+            if(err) {
+                next();
+            } else {
+                console.log(decodedToken);
+                res.status(400).json({message: "You are already Logged In!"})
+            }
+        });
         
     } else {
         next();
